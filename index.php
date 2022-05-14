0b21f162978e0ab5d9bd4af090aa273c133b3368
DeveloperNora <yerko.fuentes@inacapmail.cl>
2022-05-14 15:30:58 -0400

    changes files index and update

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
     <title>Telefonos</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS v5.0.2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Bootstrap Icon -->
@@ -36,13 +35,13 @@
                   </header>
               </div>
               <div class="col-md">
-                <header class="my-0">
-                <p id="ubicacion" class="text-left">santiago de chile.</p>
+                <header class="py-2">
+                <h6 id="ubicacion" class="text-left"> </h6>
                 </header>
               </div>
               <div class="col-md">
-                <header class="my-0">
-                <p id="temperatura" class="text-rigth">temperaura</p>
+                <header class="py-2">
+                <h6 id="temperatura" class="text-rigth"> </h6>
                 </header>
               </div>
           </div>
@@ -194,5 +193,6 @@
     integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" 
     integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="api/app.js"></script>
   </body>
 </html>
