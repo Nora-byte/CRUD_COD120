b8b37ecaa0606ce58b9aa039cff3fab80c95ef5f
DeveloperNora <yerko.fuentes@inacapmail.cl>
2022-05-14 12:38:44 -0400

    Changes Index and update

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
-<?php include 'template/header.php' ?>
-
 <?php 
   include_once "model/conextion.php";
   $sentencia = $bd -> query("select * from telefonos");
   $telefonos = $sentencia->fetchAll(PDO::FETCH_OBJ);
   //print_r($telefonos);
 ?>
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Telefonos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap Icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+  </head>
+  <body>
+      <div class="container-fluid bg-warning">
+          <div class="row">
+              <div class="col-md">
+                  <header class="py-2">
+                    <h3 class="text-center">Telefonos</h3>
+                  </header>
+              </div>
+              <div class="col-md">
+                  <header class="py-2">
+                    <h3 class="text-center"> </h3>
+                  </header>
+              </div>
+              <div class="col-md">
+                  <header class="py-2">
+                    <h3 class="text-center"> </h3>
+                  </header>
+              </div>
+              <div class="col-md">
+                <header class="my-0">
+                <p id="ubicacion" class="text-left">santiago de chile.</p>
+                </header>
+              </div>
+              <div class="col-md">
+                <header class="my-0">
+                <p id="temperatura" class="text-rigth">temperaura</p>
+                </header>
+              </div>
+          </div>
+      </div>
 
 <div class="container mt-5">
   <div class="row justify-content-center">
@@ -142,5 +183,16 @@
     </div>
   </div>
 </div>
-
-<?php include 'template/footer.php' ?>
+<footer class="container-fluid bg-dark fixed-bottom">
+      <div class="row">
+        <div class="col-md text-light text-center py-1">
+           Yerko fuentes
+        </div>
+      </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" 
+    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" 
+    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
